98fe1b449cf8a772e8ba351c58a1800aba7e1056
jcalmeida06 <162847698+jcalmeida06@users.noreply.github.com>
2025-06-04 09:10:27 -0300

    Delete automoveis directory

--- automoveis/src/main/java/com/projeto/automoveis/repository/CarrosRepository.java
+++
@@ -1,7 +0,0 @@
-package com.projeto.automoveis.repository;
-
-import com.projeto.automoveis.models.CarrosModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CarrosRepository extends JpaRepository<CarrosModel, Long> {
-}
